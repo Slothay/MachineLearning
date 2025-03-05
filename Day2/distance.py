99da069233a77604b6c243f718a0fd8263d9a828
Salla Kivistö <kivistosalla@gmail.com>
2025-03-05 15:07:40 +0200

    Done exercises

---
+++ Day2/distance.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def distance(x1,y1,x2,y2):
+    point_distance = np.sqrt((y2-y1)**2 + (x2-x1)**2)
+
+    return point_distance
+
+x1=0
+x2=3
+y1=0
+y2=4
+
+print(distance(x1,y1,x2,y2))
